626eb1fd90d830ea9242f57175d555c057db667c
sambradbury <sbradbury@unchained.com>
2023-02-07 13:29:21 -0600

    Set custom theme

--- streamlit/signing_times/device_signing.py
+++ streamlit/signing_times/device_signing.py
@@ -9,8 +9,34 @@ csv_path = os.path.join(dir_path, 'streamlit/signing_times/data/signing_data.csv
 source_data = pd.read_csv(csv_path)
 
 test_strategies = source_data['Test Strategy'].unique().tolist()
+test_strategies.pop(4) # Excludes null value from list
 devices = source_data['Model Name'].unique().tolist()
 devices.pop(7) # Excludes null value from list
+
+device_colors = {'Model One': '#00cc00', 'Model T': '#006600', 'Nano S': '#99ccff','Nano X': '#0099ff','Nano S+': '#0000ff','Mk3': '#ff6666','Mk4': '#cc0000'}
+
+def makeFig(test_strategy, model_names, y_ax_metric, y_data, title):
+    filtered_data = source_data.loc[
+        source_data['Test Strategy'].isin([test_strategy])
+        & source_data['Model Name'].isin(model_names)]
+    fig = px.line(
+        filtered_data,
+        x=y_ax_metric,
+        y=y_data,
+        color='Model Name',
+        color_discrete_map=device_colors
+        )
+    fig.update_xaxes(
+        range=[10,510],
+        tickvals=[25,50,100,250,500])
+    fig.update_layout(
+        autosize=True,
+        title={
+            'text' : title,
+            'xanchor': 'center'
+            }
+        )
+    return fig
 
 with st.form(key='viz_settings'):
     test_strategy = st.selectbox("Test Strategy:", test_strategies, index=0)
@@ -20,42 +46,11 @@ with st.form(key='viz_settings'):
     submit_button = st.form_submit_button()
 
 if submit_button:
-    pre_conf_data = source_data.loc[
-                                source_data['Test Strategy'].isin([test_strategy])
-                                & source_data['Model Name'].isin(model_names)]
-    pre_fig = px.line(
-                    pre_conf_data,
-                    x=y_ax_metric,
-                    y='Pre-Confirmation Duration (minutes)',
-                    color='Model Name'
-                    )
-    pre_fig.update_xaxes(type='category')
-    pre_fig.update_layout(
-        autosize=True,
-        title={
-            'text' : 'Pre-Confirmation Duration',
-            'xanchor': 'center'
-            }
-        )
+
+    pre_fig = makeFig(test_strategy, model_names, y_ax_metric, 'Pre-Confirmation Duration (minutes)', 'Pre-Confirmation Duration')
 
     st.plotly_chart(pre_fig, use_container_width=True)
 
-    post_conf_data = source_data.loc[
-                                source_data['Test Strategy'].isin([test_strategy])
-                                & source_data['Model Name'].isin(model_names)]
-    post_fig = px.line(
-                    post_conf_data,
-                    x=y_ax_metric,
-                    y='Post-Confirmation Duration (minutes)',
-                    color='Model Name'
-                    )
-    post_fig.update_xaxes(type='category')
-    post_fig.update_layout(
-        autosize=True,
-        title={
-            'text' : 'Post-Confirmation Duration',
-            'xanchor': 'center'
-            }
-        )
+    post_fig = makeFig(test_strategy, model_names, y_ax_metric, 'Post-Confirmation Duration (minutes)', 'Post-Confirmation Duration')
 
     st.plotly_chart(post_fig, use_container_width=True)
