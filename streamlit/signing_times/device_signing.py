8f28359cc938968e1ca2df276813513dfc81c660
sambradbury <sbradbury@unchained.com>
2023-02-07 15:59:10 -0600

    Add markers and fix .csv

--- streamlit/signing_times/device_signing.py
+++ streamlit/signing_times/device_signing.py
@@ -8,10 +8,8 @@ dir_path = os.getcwd()
 csv_path = os.path.join(dir_path, 'streamlit/signing_times/data/signing_data.csv')
 source_data = pd.read_csv(csv_path)
 
-test_strategies = source_data['Test Strategy'].unique().tolist()
-test_strategies.pop(4) # Excludes null value from list
-devices = source_data['Model Name'].unique().tolist()
-devices.pop(7) # Excludes null value from list
+test_strategies = source_data['Test Strategy'].dropna().unique().tolist()
+devices = source_data['Model Name'].dropna().unique().tolist()
 
 device_colors = {'Model One': '#00cc00', 'Model T': '#006600', 'Nano S': '#99ccff','Nano X': '#0099ff','Nano S+': '#0000ff','Mk3': '#ff6666','Mk4': '#cc0000'}
 
@@ -19,16 +17,23 @@ def makeFig(test_strategy, model_names, y_ax_metric, y_data, title):
     filtered_data = source_data.loc[
         source_data['Test Strategy'].isin([test_strategy])
         & source_data['Model Name'].isin(model_names)]
+
+    ticks = filtered_data[y_ax_metric].dropna().unique().tolist()
+    x_min = min(ticks) * .5
+    x_max = max(ticks) * 1.05
+
     fig = px.line(
         filtered_data,
         x=y_ax_metric,
         y=y_data,
+        markers=True,
         color='Model Name',
         color_discrete_map=device_colors
         )
     fig.update_xaxes(
-        range=[10,510],
-        tickvals=[25,50,100,250,500])
+        range=[x_min,x_max],
+        tickvals=ticks
+        )
     fig.update_layout(
         autosize=True,
         title={
